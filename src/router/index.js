d74d5e6654de310d9e0c1383dc979e0d780ea5d1
Your Name <erguloguz@hotmail.com>
2021-07-10 15:41:59 +0300

    Add: Related routes has been created.

--- src/router/index.js
+++ src/router/index.js
@@ -5,25 +5,32 @@ import Home from '../views/Home.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/user/:id',
+        name: 'UserDetail',
+        component: () => import(/* webpackChunkName: "about" */ '../views/UserDetail.vue')
+    },
+    {
+        path: '/post/:id',
+        name: 'PostDetail',
+        component: () => import(/* webpackChunkName: "about" */ '../views/PostDetail.vue')
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'hash',
+    base: process.env.BASE_URL,
+    routes
+})
+
+
+router.afterEach(() => {
+    window.scrollTo(0, 0);
 })
 
 export default router
