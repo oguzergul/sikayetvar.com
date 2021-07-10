b1a3aae80a1da1ac3b494944f9b78b517694bc48
Your Name <erguloguz@hotmail.com>
2021-07-10 15:45:39 +0300

    Add: SVG icons exported as default. Global styles declaration completed.

---
+++ src/assets/icons/index.js
@@ -0,0 +1,6 @@
+export {default as IconTwitter} from '../icons/twitter.svg';
+export {default as IconDribble} from '../icons/dribble.svg';
+export {default as IconFacebook} from '../icons/facebook.svg';
+export {default as IconGithub} from '../icons/github.svg';
+export {default as IconLogo} from '../icons/logo.svg';
+export {default as IconClose} from '../icons/close.svg';
---
+++ src/assets/units/index.js
@@ -0,0 +1,2 @@
+export {default as BackgroundCover} from '../units/background-cover.svg';
+export {default as BackgroundOval} from '../units/oval.svg';
