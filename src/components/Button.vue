91386d3d60c80d650cc29b382c4afbab7806c546
Your Name <erguloguz@hotmail.com>
2021-07-10 15:40:35 +0300

    Add: Button, Social Button,Alert,Modal,Container Card, Header and Footer components created.

---
+++ src/components/Button.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button :style="{backgroundColor:_button_type}" :type="type" class="regular-button"
+            @click="$emit('onClick',$event)">
+      <span>{{ title }}</span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: ['type', 'title'],
+  computed: {
+    _button_type() {
+      const type = this.type;
+      if (type === 'detail') {
+        return '#5E72E4';
+      } else if (type === 'delete') {
+        return '#FB6340';
+      } else if (type === 'edit') {
+        return '#2DCE89';
+      } else if (type === 'update') {
+        return '#11CDEF';
+      } else if (type === 'login') {
+        return '#FFFFFF';
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/SocialButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <a :href="route" :style="{backgroundColor:_media_button}" class="social-media-button" target="_blank">
+    <slot/>
+    <span class="social-media-button-popover">Follow us</span>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'SocialButton',
+  props: ["type", "route"],
+  computed: {
+    _media_button() {
+      const buttonTypes = [
+        {id: 1, type: 'facebook', color: '#3B5999'},
+        {id: 2, type: 'twitter', color: '#1DA1F2'},
+        {id: 3, type: 'dribble', color: '#EA4C89'},
+        {id: 4, type: 'github', color: '#222222'},
+      ]
+      return buttonTypes.find(types => types.type === this.type).color;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
