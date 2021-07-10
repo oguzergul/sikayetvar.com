743410156aa6d8ee82842a8b6f721772a5f99959
Your Name <erguloguz@hotmail.com>
2021-07-10 15:38:46 +0300

    Add: SVG icons has been included and used as Component.

---
+++ src/components/BackgroundCover.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="background-cover">
+    <BackgroundCover class="background-cover-base"/>
+
+    <div class="background-cover-oval-container">
+      <BackgroundOval
+          v-for="(oval,index) in 7"
+          :key="index"
+          class="background-cover-oval-container-item"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import {BackgroundCover, BackgroundOval} from "../assets/units";
+
+export default {
+  name: 'Background',
+  components: {
+    BackgroundCover,
+    BackgroundOval
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
