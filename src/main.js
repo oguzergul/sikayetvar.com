dfe6d3316dd0d65400bd62cf870f47349ae19b9d
Your Name <erguloguz@hotmail.com>
2021-07-10 15:46:12 +0300

    Add: SVG loader configuration file added. Npm modules declared.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import x5GMaps from 'x5-gmaps';
+import './styles/index.scss';
+Vue.config.productionTip = false
+Vue.use(x5GMaps, 'AIzaSyBGhmoLOiZrA1QckOR0nPIXMjf01BgyIVk');
+
 
 Vue.config.productionTip = false
 
